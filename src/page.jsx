d85370613c61e9165e5dd2a7a6f11faa3f19dae2
shivamtripathi <2002shiv01@gmail.com>
2025-03-30 23:19:34 +0530

    Added new feature to the app

---
+++ src/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Shivam </h1>
+            <p>Thiii...........</p>
+        </div>
+    );
+};
+
+export default Page;
